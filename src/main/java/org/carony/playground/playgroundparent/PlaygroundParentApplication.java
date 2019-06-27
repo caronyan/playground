d440d1afb71b8dfa9b7c485391f9a29e27787354
caron yan <carony@163.com>
2019-06-27 15:35:47 +0800

    feat(init): spring init

---
+++ src/main/java/org/carony/playground/playgroundparent/PlaygroundParentApplication.java
@@ -0,0 +1,13 @@
+package org.carony.playground.playgroundparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaygroundParentApplication.class, args);
+	}
+
+}
